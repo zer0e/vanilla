d473dc0fee6769fcf6440a5a39499e93fbc8edc9
zer0e <zcluslloye@gmail.com>
2024-06-26 19:37:48 +0800

    feat(all): 增加了后台框架结构

---
+++ vanilla-backend/src/main/java/com/github/zer0e/vanilla/VanillaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.zer0e.vanilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanillaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanillaBackendApplication.class, args);
+    }
+
+}
---
+++ vanilla-backend/src/test/java/com/github/zer0e/vanilla/VanillaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zer0e.vanilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanillaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
